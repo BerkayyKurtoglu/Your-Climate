42067ab0a3d860e2957482ed3ac6324671d503ef
Berkay Kurtoglu <berkayberkay300@hotmail.com>
2021-09-12 14:50:29 +0300

    First uploaded time

--- app/src/main/java/com/example/yourclimate/util/ServiceObject.kt
+++
@@ -1,8 +0,0 @@
-package com.example.yourclimate.util
-
-object ServiceObject {
-    const val API_KEY = "2df59dc9b86acc19c838cd22343c7fdf"
-    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val LAT = "41.0828926"
-    const val LONG = "28.9437645"
-}
